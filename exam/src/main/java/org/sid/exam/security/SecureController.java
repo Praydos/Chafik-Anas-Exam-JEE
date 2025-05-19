66352c0a0366895dd8a47067baf1676b3d1f2a39
Praydos <chafik.anass2017@gmail.com>
2025-05-19 11:49:35 +0100

    last up

--- exam/src/main/java/org/sid/exam/security/SecureController.java
+++ exam/src/main/java/org/sid/exam/security/SecureController.java
@@ -1,21 +1,21 @@
-package org.sid.exam.security;
-
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/secure")
-public class SecureController {
-
-    @GetMapping("/user")
-    @PreAuthorize("hasRole('USER')")
-    public String userAccess() {
-        return "This is a USER-only endpoint.";
-    }
-
-    @GetMapping("/admin")
-    @PreAuthorize("hasRole('ADMIN')")
-    public String adminAccess() {
-        return "This is an ADMIN-only endpoint.";
-    }
-}
+//package org.sid.exam.security;
+//
+//import org.springframework.security.access.prepost.PreAuthorize;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/secure")
+//public class SecureController {
+//
+//    @GetMapping("/user")
+//    @PreAuthorize("hasRole('USER')")
+//    public String userAccess() {
+//        return "This is a USER-only endpoint.";
+//    }
+//
+//    @GetMapping("/admin")
+//    @PreAuthorize("hasRole('ADMIN')")
+//    public String adminAccess() {
+//        return "This is an ADMIN-only endpoint.";
+//    }
+//}
